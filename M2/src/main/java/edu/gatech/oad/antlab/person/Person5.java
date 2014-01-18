af7eaa0f4633f44111b906d28c1d59c649627769
R3mak <rmcbride6@gatech.edu>
2014-01-18 17:05:22 -0500

    Revert "Robert's M2 Files"
    This reverts commit 04c0f9b14cce9d729d60d6cefd36eb4d08596bb1.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,8 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  String str = input.substring(0, 2);
-	  return input.substring(2) + str;
+	  //Person 5 put your implementation here
+	  return null;
 	}
 	
 	/**
